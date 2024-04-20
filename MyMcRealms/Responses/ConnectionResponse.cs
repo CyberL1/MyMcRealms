075af61c9f9cd9fc518480335f54432b1c892fa5
CyberL1 <mateusz.hf18@gmail.com>
2024-04-20 19:42:22 +0200

    Fetch all my-mc servers

---
+++ MyMcRealms/Responses/ConnectionResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace MyMcRealms.Responses
+{
+    public class ConnectionResponse
+    {
+        public string Address { get; set; } = string.Empty;
+        public bool PendingUpdate { get; set; }
+    }
+}
