355b4792a12a6e6d63e7bab337d0465c13ac4722
CyberL1 <cyber.hf18@gmail.com>
2024-04-20 11:37:26 +0200

    chrore: group mymc api to one directory

---
+++ MyMcRealms/MyMcAPI/Responses/HelloResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Minecraft_Realms_Emulator.MyMcAPI.Responses
+{
+    public class HelloResponse
+    {
+        public string Message { get; set; } = string.Empty;
+    }
+}
--- MyMcRealms/MyMcAPIResponses/MyMcHelloResponse.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Minecraft_Realms_Emulator.MyMcAPIResponses
-{
-    public class MyMcHelloResponse
-    {
-        public string Message { get; set; } = string.Empty;
-    }
-}
