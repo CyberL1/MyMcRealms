207c14417c19bbff5c8c879f2b9922bf49f7565f
CyberL1 <cyber.hf18@gmail.com>
2024-04-22 18:56:14 +0200

    feat: server id as realm name

--- MyMcRealms/MyMcAPI/Responses/AllServersResponse.cs
+++ MyMcRealms/MyMcAPI/Responses/AllServersResponse.cs
@@ -5,17 +5,12 @@ namespace MyMcRealms.MyMcAPI.Responses
     {
         public bool Success { get; set; }
         public List<Server> Servers { get; set; }
-
-        public static implicit operator Task<object>(AllServersResponse? v)
-        {
-            throw new NotImplementedException();
-        }
     }
 
     public class Server
     {
         public string ServerName { get; set; } = string.Empty;
         public string Connect { get; set; } = string.Empty;
-        public string GameVersion {  get; set; } = string.Empty;
+        public string GameVersion { get; set; } = string.Empty;
     }
 }
