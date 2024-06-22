57d4250a863eabc40591ba98f63ca06c2a02a3fe
CyberL1 <cyber.hf18@gmail.com>
2024-06-22 07:33:39 +0200

    feat: live player list

---
+++ MyMcRealms/Controllers/ActivitiesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMcRealms.Attributes;
+using MyMcRealms.Helpers;
+using MyMcRealms.MyMcAPI;
+using MyMcRealms.Responses;
+using Newtonsoft.Json;
+
+namespace MyMcRealms.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [RequireMinecraftCookie]
+    public class ActivitiesController : ControllerBase
+    {
+        [HttpGet("liveplayerlist")]
+        public async Task<ActionResult<LivePlayerListsResponse>> GetLivePlayerList()
+        {
+            string cookie = Request.Headers.Cookie;
+            string playerUUID = cookie.Split(";")[0].Split(":")[2];
+
+            List<LivePlayerList> lists = [];
+
+            var allServers = await new Wrapper(Environment.GetEnvironmentVariable("MYMC_API_KEY")).GetAllServers();
+
+            foreach (var world in allServers.Servers)
+            {
+                var query = new MinecraftServerQuery().Query(world.Connect);
+
+                if (query == null) continue;
+
+                List<object> players = [];
+
+                if (query.Players.Sample == null) continue;
+
+                foreach (var player in query.Players.Sample)
+                {
+                    players.Add(new
+                    {
+                        playerId = player.Id.Replace("-", ""),
+                    });
+                }
+
+                LivePlayerList list = new()
+                {
+                    ServerId = allServers.Servers.IndexOf(world),
+                    PlayerList = JsonConvert.SerializeObject(players),
+                };
+
+                lists.Add(list);
+            };
+
+            LivePlayerListsResponse response = new()
+            {
+                Lists = lists
+            };
+
+            return Ok(response);
+        }
+    }
+}
